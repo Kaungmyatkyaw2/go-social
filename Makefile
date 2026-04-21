42ea37d319409f9d842c0abe8ba64d305c698b0f
Kaungmyatkyaw2 <kaungmyatkyaw2030@gmail.com>
2026-04-21 16:32:14 +0630

    first commit

---
+++ Makefile
@@ -0,0 +1,23 @@
+ifneq ("$(wildcard .env)","")
+    include .env
+    export $(shell sed 's/=.*//' .env)
+endif
+
+MIGRATIONS_PATH = ./cmd/migrate/migrations
+
+.PHONY: migrate-create
+migration:
+	@migrate create -seq -ext sql -dir $(MIGRATIONS_PATH) $(filter-out $@,$(MAKECMDGOALS))
+
+.PHONY: migrate-up
+migrate-up:
+	@migrate -path=$(MIGRATIONS_PATH) -database=$(DB_DSN) up
+
+.PHONY: migrate-down
+migrate-down:
+	@migrate -path=$(MIGRATIONS_PATH) -database=$(DB_DSN) down $(filter-out $@,$(MAKECMDGOALS))
+
+.PHONY: fmt
+fmt:
+	@echo "Formatting Go files..."
+	@go fmt ./...
---
+++ cmd/api/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+
+	"github.com/Kaungmyatkyaw2/go-social/internal/db"
+	"github.com/Kaungmyatkyaw2/go-social/internal/env"
+	"github.com/Kaungmyatkyaw2/go-social/internal/store"
+	"github.com/joho/godotenv"
+)
+
+const version = "0.0.1"
+
+func main() {
+
+	godotenv.Load()
+
+	cfg := config{
+		addr: env.GetString("ADDR", ":8080"),
+		env:  env.GetString("ENV", "development"),
+		db: dbConfig{
+			dsn:          env.GetString("DB_DSN", "postgres://admin:adminpassword@localhost:5435/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
+	}
+
+	log.Println(cfg.db.dsn)
+
+	db, err := db.New(cfg.db.dsn, cfg.db.maxOpenConns, cfg.db.maxOpenConns, cfg.db.maxIdleTime)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Println("database connection pool established")
+
+	store := store.NewStorage(db)
+
+	app := &application{
+		config: cfg,
+		store:  store,
+	}
+
+	mux := app.mount()
+
+	log.Fatal(app.run(mux))
+}
