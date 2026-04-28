dda4b6e8df70e3fdaec70bc45ad43ad27f0551be
Kaungmyatkyaw2 <kaungmyatkyaw2030@gmail.com>
2026-04-28 12:26:05 +0630

    feat: add docs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,15 +9,32 @@ import (
 	"github.com/joho/godotenv"
 )
 
-const version = "0.0.1"
+const version = "0.0.2"
 
+//	@title			GoSocial API
+//	@description	API for GOSocial Project
+//	@termsOfService	http://swagger.io/terms/
+
+//	@contact.name	API Support
+//	@contact.url	http://www.swagger.io/support
+//	@contact.email	support@swagger.io
+
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+
+//	@BasePath	/v1
+
+// @securityDefinitions.apikey	ApiKeyAuth
+// @in							header
+// @name						Ahutorization
 func main() {
 
 	godotenv.Load()
 
 	cfg := config{
-		addr: env.GetString("ADDR", ":8080"),
-		env:  env.GetString("ENV", "development"),
+		addr:   env.GetString("ADDR", ":8080"),
+		env:    env.GetString("ENV", "development"),
+		apiURL: env.GetString("API_URL", "localhost:8080"),
 		db: dbConfig{
 			dsn:          env.GetString("DB_DSN", "postgres://admin:adminpassword@localhost:5435/social?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
